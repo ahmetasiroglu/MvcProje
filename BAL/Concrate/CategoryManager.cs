02f05450146fa14ba8bdd3b5c024193c8f29eb4b
Ahmet Aşıroğlu <136030857+ahmetasiroglu@users.noreply.github.com>
2024-11-23 18:30:55 +0300

    Add project files.

---
+++ BAL/Concrate/CategoryManager.cs
@@ -0,0 +1,33 @@
+﻿using DAL.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.Concrate
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+
+        public List<Category> GetAll()
+        {
+            return repo.GetList();
+        }
+
+        public void CategoryAddBl(Category p)
+        {
+            if(p.CatagoryName == "" || p.CatagoryName.Length<=3 || p.CatagoryDescriptions == "" || p.CatagoryName.Length>=51)
+            {
+                //hata mesajı
+            }
+
+            else
+            {
+                repo.Insert(p);
+            }
+        }
+    }
+}
---
+++ DAL/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,44 @@
+﻿using DAL.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Concrete.Repositories
+{
+    public class CategoryRepository : GenericRepository<Category>
+    {
+        Context c = new Context();
+        DbSet<Category> _object;
+        public void Delete(Category p)
+        {
+            _object.Remove(p);
+            c.SaveChanges();
+        }
+
+        public List<Category> GetList()
+        {
+            return _object.ToList();
+        }
+
+        public void Insert(Category p)
+        {
+            _object.Add(p);
+            c.SaveChanges();
+        }
+
+        public List<Category> List(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category p)
+        {
+            c.SaveChanges();
+        }
+    }
+}
